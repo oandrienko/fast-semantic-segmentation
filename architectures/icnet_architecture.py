fix filter scaling for icnet

architectures/icnet_architecture.py
@@ -124,32 +124,33 @@ class ICNetArchitecture(model.FastSegmentationModel):
                                 size=(input_h, input_w))
             branch_merge = tf.add_n([input_features, full_pool,
                                      half_pool, third_pool, quarter_pool])
-        output = slim.conv2d(branch_merge, 256, [1, 1], stride=1,
+        output = slim.conv2d(branch_merge, 512//self._filter_scale, [1, 1], stride=1,
                                  normalizer_fn=slim.batch_norm,
                                  scope='Conv_1x1')
         return output
 
     def _third_feature_branch(self, preprocessed_inputs):
-        net = slim.conv2d(preprocessed_inputs, 32, [3,3], stride=2,
-             normalizer_fn=slim.batch_norm)
-        net = slim.conv2d(net, 32, [3,3], stride=2,
-             normalizer_fn=slim.batch_norm)
-        net = slim.conv2d(net, 64, [3,3], stride=2,
-             normalizer_fn=slim.batch_norm)
-        output = slim.conv2d(net, 128, [3,3], stride=1,
-             normalizer_fn=slim.batch_norm)
+        net = slim.conv2d(preprocessed_inputs, 64//self._filter_scale, [3,3],
+                stride=2, normalizer_fn=slim.batch_norm)
+        net = slim.conv2d(net, 64//self._filter_scale, [3,3],
+                stride=2, normalizer_fn=slim.batch_norm)
+        net = slim.conv2d(net, 128//self._filter_scale, [3,3],
+                stride=2, normalizer_fn=slim.batch_norm)
+        output = slim.conv2d(net, 256//self._filter_scale, [3,3],
+                stride=1, normalizer_fn=slim.batch_norm)
         return output
 
     def _cascade_feature_fusion(self, first_feature_map,
                                 second_feature_map, scope):
         with tf.variable_scope(scope):
             upsampled_inputs = self._dynamic_interpolation(first_feature_map, z_factor=2)
-            dilated_conv = slim.conv2d(upsampled_inputs, 128, [3, 3],
-                                       stride=1, rate=2,
+            dilated_conv = slim.conv2d(upsampled_inputs, 256//self._filter_scale,
+                                      [3, 3], stride=1, rate=2,
                                        normalizer_fn=slim.batch_norm,
                                        activation_fn=None,
                                        scope="DilatedConv")
-            conv = slim.conv2d(second_feature_map, 128, [1, 1], stride=1,
+            conv = slim.conv2d(second_feature_map, 256//self._filter_scale,
+                               [1, 1], stride=1,
                                normalizer_fn=slim.batch_norm,
                                activation_fn=None,
                                scope="Conv")

train.py
@@ -120,7 +120,6 @@ def create_training_model_losses(input_queue, create_model_fn, train_config):
     segmentation_model.provide_groundtruth(labels)
     prediction_dict = segmentation_model.predict(images)
 
-    # output graph.pbtxt
     # with tf.Session() as sess:
     #     train_writer = tf.summary.FileWriter('./tmp')
     #     train_writer.add_graph(sess.graph)
@@ -205,7 +204,8 @@ def train_segmentation_model(create_model_fn,
               optimizer_summary_vars) = optimizer_builder.build(
                 train_config.optimizer)
             for var in optimizer_summary_vars:
-                tf.summary.scalar(var.op.name, var, family='LearningRate')
+                summaries.add(
+                    tf.summary.scalar(var.op.name, var, family='LearningRate'))
 
         # Add summaries for model variables.
         for model_var in slim.get_model_variables():